62866127f18a85380709a8d92b4956d03758d368
Henry AT <yiperu@gmail.com>
2013-10-05 20:32:14 -0500

    Complementamos el archivo adicionando una linea de codigo que permitira mostrar en consola el mensaje que le indiquemos

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
 var servidor = require('http'); //el metodo requiereo el modulo http
 
 function arrancaServidor(requiere, respuesta) {
+    console.log("Alguien se ah conectado");
     respuesta.writeHead(200,{"Content-Type":"text/html"}); // le digo que le enviare informacion de tipo html al servidor
     respuesta.write("<h1>El servidor funciona correctamente en el puerto 8888</h1>");
     respuesta.end();
